56a811884165d1f774c498ce7bf6640e9bc975ea
andipahlevy <andilevi@gmail.com>
2020-09-23 21:28:07 +0700

    oke

--- app/Http/Controllers/Eks/HomeController.php
+++ app/Http/Controllers/Eks/HomeController.php
@@ -98,7 +98,7 @@ class HomeController extends Controller
 		$file = new Google_Service_Drive_DriveFile();
 		$file->setParents(["1niTIZygrK9EG0RBritmsPvJCMBy4FpCF",$folderId]);
 		$optParams = array(
-			'q'		 => "'$folderId' in parents and trashed=false",
+			'q'		 => "'$folderId' in parents and mimeType contains 'audio' and trashed=false",
 			'spaces' => "drive",
 			'fields' => 'nextPageToken, files(id, name)'
 		);
