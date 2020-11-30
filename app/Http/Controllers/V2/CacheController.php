a110c95038bee96618da8d73c69ad3888ab98107
andipahlevy <andilevi@gmail.com>
2020-11-30 19:29:49 +0700

    v2

---
+++ app/Http/Controllers/V2/CacheController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Eks;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+
+class CacheController extends Controller
+{
+	
+    public function __construct()
+    {
+    }
+	
+	
+	public function clear()
+	{
+		Cache::flush();
+		echo 'cache clear';
+	}
+	
+	public function alias($url)
+	{
+		$img = base64_decode($url);
+		$fp = fopen($img, 'rb');
+
+		header('Content-type: image/jpeg;');
+		foreach ($http_response_header as $h) {
+			if (strpos($h, 'Content-Length:') === 0) {
+				header($h);
+				break;
+			}
+		}
+
+		fpassthru($fp);
+		exit;
+	}
+	
+}
